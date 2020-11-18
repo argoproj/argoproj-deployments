f3e5b371625ac8c96b00a8db43aec6e8e21aeac1
Jesse Suen <jesse_suen@intuit.com>
2020-11-18 00:17:31 -0800

    chore: upgrade prometheus-operator
    Signed-off-by: Jesse Suen <jesse_suen@intuit.com>

--- prometheus-operator/upstream.sh
+++ prometheus-operator/upstream.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 helm dependency update upstream
-helm template --set grafana.sidecar.dashboards.enabled=true --name prometheus-operator --namespace prometheus-operator upstream > resources/upstream.yaml
+helm template \
+    --namespace prometheus-operator \
+    prometheus-operator \
+    ./upstream > resources/upstream.yaml
