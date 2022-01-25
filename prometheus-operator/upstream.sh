d64a2709221dca6eebcb1256f6a9118b88be12d8
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2022-01-24 22:53:55 -0800

    include prometheus crds
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- prometheus-operator/upstream.sh
+++ prometheus-operator/upstream.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 helm dependency update upstream
 helm template \
+    --include-crds \
     --namespace prometheus-operator \
     prometheus-operator \
     ./upstream > resources/upstream.yaml
