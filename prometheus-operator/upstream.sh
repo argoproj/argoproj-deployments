234acfd325aa36700761a724e05305095aa5af7e
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2025-06-16 14:21:45 -0400

    feat: add priorityClass to stabilize important apps (#37)
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- prometheus-operator/upstream.sh
+++ prometheus-operator/upstream.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 helm dependency update upstream
 helm template \
+    -f ./upstream/values.yaml \
     --include-crds \
     --namespace prometheus-operator \
     prometheus-operator \
